32ea6042ed75d9e9fa1bccb9a6bd9a7e57af214b
Obi Onyemaobi <obinnaonye@gmail.com>
2018-07-22 19:00:41 +0000

    Upload New File

---
+++ Project Delta/main.js
@@ -0,0 +1,17 @@
+const axios = require('axios');
+const botToken = require("./bot-token.json");
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+
+client.login(botToken.token);
